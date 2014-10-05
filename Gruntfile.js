0a65186b19439a3a17e2e1047de8e90e457afcc8
James Warren <james.warren@spectrakey.co.uk>
2014-10-05 23:45:11 +0100

    Grunt: change livereload hostname to allow outside access

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,11 +53,11 @@ module.exports = function(grunt) {
         port: 9000,
         livereload: 35729,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
-          open: true,
+          open: 'http://localhost:9000',
           base: [
             '<%= config.dist %>'
           ]
