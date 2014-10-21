b57311f27db567e87eb22df4db969a4fd85fc573
Andy Warren <andrew.warren@spectrakey.co.uk>
2014-10-21 14:04:38 +0100

    fixed message POST

--- src/lib/sendmail.php
+++ src/lib/sendmail.php
@@ -1,7 +1,7 @@
 <?php
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
-if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['text']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['msg']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
   // detect & prevent header injections
   $test = "/(content-type|bcc:|cc:|to:)/i";
